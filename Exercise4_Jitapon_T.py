df12d73331cf1fc1ccb9bdd95fd771a75f269400
Byu J <67407908+Byujita@users.noreply.github.com>
2022-06-08 15:58:03 +0700

    Create Exercise4_Jitapon_T.py

---
+++ Exercise4_Jitapon_T.py
@@ -0,0 +1,10 @@
+fe = 91
+gb = 93
+ics = 95
+cp = 97
+
+print("---Your Score---")
+print("Foundation English :", "คะแนนที่ได้", fe)
+print("General Business :", "คะแนนที่ได้", gb)
+print("Introduction to Computer Systems :", "คะแนนที่ได้", ics)
+print("Computer Programming :", "คะแนนที่ได้", cp)
